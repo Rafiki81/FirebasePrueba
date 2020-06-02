3e9bef7fed84e3689f2b1adaa8552dbe7798b995
Rafiki <rperezbeato@gmail.com>
2020-06-02 21:49:39 +0200

    Creacion del metodo getPelicula

--- app/src/main/java/com/rafiki/firebaseprueba/MainActivity.java
+++ app/src/main/java/com/rafiki/firebaseprueba/MainActivity.java
@@ -1,13 +1,18 @@
 package com.rafiki.firebaseprueba;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -42,12 +47,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void updatePelicula(View view){
-
+        DatabaseReference peliculasRef = myRef.child("Peliculas");
+        Pelicula pelicula = crearPelicula();
+        pelicula.setOriginalTitle("La nueva Pelicula");
+        HashMap<String,Object> peliculas = new HashMap<>(); //Objeto clave valor, clave el id, valor el objeto, en este caso la pelicula
+        peliculas.put(String.valueOf(pelicula.getId()),pelicula);
+        //añadimos la pelicula con su id como nodo.
+        peliculasRef.updateChildren(peliculas);
 
     }
 
-    public void getPelicula(View view){
+    public void getPelicula(View view) {
+        DatabaseReference peliculasRef = myRef.child("Peliculas").child("419704");
+        peliculasRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Pelicula pelicula = dataSnapshot.getValue(Pelicula.class);
+                System.out.println(pelicula.toString());
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println("Error al recuperar los datos");
+            }
+        });
 
     }
 
