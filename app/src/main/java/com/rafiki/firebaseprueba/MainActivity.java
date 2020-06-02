5cc42d869d8aeb84d2ec9661d8edd1851f91d4d8
Rafiki <rperezbeato@gmail.com>
2020-06-02 21:32:04 +0200

    Creacion de APP conectada a Firebase y prueba de alta de peliculas.

---
+++ app/src/main/java/com/rafiki/firebaseprueba/MainActivity.java
@@ -0,0 +1,78 @@
+package com.rafiki.firebaseprueba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+        //Crea un registro dentro de la coleccion message
+       // FirebaseDatabase database = FirebaseDatabase.getInstance();
+       // DatabaseReference myRef = database.getReference("message");
+       // myRef.setValue("Hello, World!");
+
+    }
+
+    public void createPelicula(View view){
+        //Creamos un nuevo nodo pelicula
+        DatabaseReference peliculasRef = myRef.child("Peliculas");
+        Pelicula pelicula = crearPelicula();
+
+        HashMap<String,Pelicula> peliculas = new HashMap<>(); //Objeto clave valor, clave el id, valor el objeto, en este caso la pelicula
+        peliculas.put(String.valueOf(pelicula.getId()),pelicula);
+        //añadimos la pelicula con su id como nodo.
+        peliculasRef.setValue(peliculas);
+
+    }
+
+    public void updatePelicula(View view){
+
+
+    }
+
+    public void getPelicula(View view){
+
+
+    }
+
+    private Pelicula crearPelicula(){
+
+        Pelicula pelicula = new Pelicula();
+
+        pelicula.setId(419704);
+        pelicula.setPopularity(53899);
+        pelicula.setVoteCount(3199);
+        pelicula.setVideo(true);
+        pelicula.setPosterPath("/gkveoprgjeriopjerwpforfdklewop`.jpg");
+        pelicula.setAdult(true);
+        pelicula.setBackdropPath("/gjreiojeiorfjfewiojgoreithoierw.jpg");
+        pelicula.setOriginalLanguage("en");
+        pelicula.setOriginalTitle("Ad Astra");
+        pelicula.setTitle("Ad Astra");
+        pelicula.setVoteAverage(6);
+        pelicula.setOverView("Roy McBride es un ingeniero que perdió a su padre en una misión sin retorno a Neptuno para encontrar signos de inteligencia extraterrestre. 20 años después, emprenderá su propio viaje a través del sistema solar para tratar de encontrarlo de nuevo y resolver los misterios del porqué esa primera misión fracasó.");
+        pelicula.setReleaseDate("2019-09-17");
+
+        pelicula.setGenreIds(new ArrayList<Integer>(){{
+            add(18);
+            add(878);
+        }});
+        return pelicula;
+    }
+}
---
+++ app/src/main/java/com/rafiki/firebaseprueba/Pelicula.java
@@ -0,0 +1,176 @@
+package com.rafiki.firebaseprueba;
+
+
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Pelicula implements Serializable {
+
+    private int id;
+    private double popularity;
+    private int voteCount;
+    private boolean video;
+    private String posterPath;
+    private boolean adult;
+    private String backdropPath;
+    private String originalLanguage;
+    private String originalTitle;
+    private List<Integer> genreIds;
+    private String title;
+    private int voteAverage;
+    private String overView;
+    private String releaseDate;
+
+    public Pelicula() {
+    }
+
+    public Pelicula(int id, double popularity, int voteCount, boolean video, String posterPath, boolean adult, String backdropPath, String originalLanguage, String originalTitle, List<Integer> genreIds, String title, int voteAverage, String overView, String releaseDate) {
+        this.id = id;
+        this.popularity = popularity;
+        this.voteCount = voteCount;
+        this.video = video;
+        this.posterPath = posterPath;
+        this.adult = adult;
+        this.backdropPath = backdropPath;
+        this.originalLanguage = originalLanguage;
+        this.originalTitle = originalTitle;
+        this.genreIds = genreIds;
+        this.title = title;
+        this.voteAverage = voteAverage;
+        this.overView = overView;
+        this.releaseDate = releaseDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Pelicula{" +
+                "id=" + id +
+                ", popularity=" + popularity +
+                ", voteCount=" + voteCount +
+                ", video=" + video +
+                ", posterPath='" + posterPath + '\'' +
+                ", adult=" + adult +
+                ", backdropPath='" + backdropPath + '\'' +
+                ", originalLanguage='" + originalLanguage + '\'' +
+                ", originalTitle='" + originalTitle + '\'' +
+                ", genreIds=" + genreIds +
+                ", title='" + title + '\'' +
+                ", voteAverage=" + voteAverage +
+                ", overView='" + overView + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(double popularity) {
+        this.popularity = popularity;
+    }
+
+    public int getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(int voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public boolean isVideo() {
+        return video;
+    }
+
+    public void setVideo(boolean video) {
+        this.video = video;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public boolean isAdult() {
+        return adult;
+    }
+
+    public void setAdult(boolean adult) {
+        this.adult = adult;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    public void setOriginalLanguage(String originalLanguage) {
+        this.originalLanguage = originalLanguage;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public List<Integer> getGenreIds() {
+        return genreIds;
+    }
+
+    public void setGenreIds(List<Integer> genreIds) {
+        this.genreIds = genreIds;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(int voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public String getOverView() {
+        return overView;
+    }
+
+    public void setOverView(String overView) {
+        this.overView = overView;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
